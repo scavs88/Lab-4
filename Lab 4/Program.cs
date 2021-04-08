46878b18c7d1d12ec8b2c94e98f3985dab8ad9da
sean <spcavanaugh88@gmail.com>
2021-04-07 23:06:03 -0400

    Lab 4

---
+++ Lab 4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loopProgram = true;
+            while (true)
+            {
+
+                Console.WriteLine("Please enter an integer");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Number      Squared       Cubed");
+                Console.WriteLine("-------------------------------");
+                CountUp(x);
+                Console.WriteLine("Would you like to see another table? y/n");
+                string answer = Console.ReadLine();
+                if (answer == "n")
+                {
+                    loopProgram = false;
+                    break;
+                }
+                if (answer == "y")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+
+                }
+            }
+
+
+
+
+        }
+
+
+
+
+
+        static void CountUp(int x)
+        {
+            for (int i = 1; i <= x; i++)
+            {
+
+                Console.WriteLine($" {i}  {Squared(i)}  {Cuber(i)}");
+
+            }
+        }
+
+        static int getInput()
+        {
+
+
+            Console.WriteLine("Please enter an integer");
+            int input = int.Parse(Console.ReadLine());
+            return input; ;
+        }
+
+        static int Cuber(int x)
+        {
+            return x * x * x;
+
+        }
+
+        static int Squared(int x)
+        {
+            return x * x;
+        }
+    }
+}
